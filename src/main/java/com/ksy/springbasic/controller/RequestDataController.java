86133c6aa7831b16972d14070fea94eeebaa06eb
workmakerdaily <sver12@naver.com>
2024-08-21 17:42:22 +0900

    24.08.21 17:40

--- src/main/java/com/ksy/springbasic/controller/RequestDataController.java
+++ src/main/java/com/ksy/springbasic/controller/RequestDataController.java
@@ -1,6 +1,7 @@
 package com.ksy.springbasic.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +20,47 @@ public class RequestDataController {
     // http://localhost:4000/request-data/request-param?name=홍길동&age=30
     public String requestParam(
         // @RequestParam(name="name") String name,
-        @RequestParam() String name,
+        @RequestParam("name") String name,
         @RequestParam(name="age", required=false) Integer age
     ) {    
         return "이름 : " + name + " 나이 : " + age;
+    }
+
+    // @PathVariable():
+    // 모든 HTTP 메서드에서 URL의 특정 패턴에 따라 데이터를 추출
+    // GET http://localhost:4000/request-data/path-variable/*/*
+    @GetMapping({
+            "/path-variable/{var}/{str}",
+            "/path-variable/{var}/",
+            "/path-variable/{var}"
+    })
+    public String pathVariable(
+        @PathVariable(name="var") String var,
+        @PathVariable(name="str", required=false) String str
+    ) {
+        return "읽은 경로 변수 : " + var + ", " + str;
+    }
+
+    // ! 주의
+    // 경로 변수를 사용하여 URL 패턴을 작성할 때
+    // 겹치는 패턴이 존재하는지 잘 확인해야함
+    @GetMapping("/path-variable/other")
+    public String otherPathVariable() {
+        return "other 메서드 호출";
+    }
+
+    @GetMapping("/path-variable/{var}/another")
+    public String anotherPathVariable1 (
+        @PathVariable("var") String var
+    ) {
+        return "another1";
+    }
+
+    @GetMapping("/path-variable/another/{var}")
+    public String anotherPathVariable2 (
+        @PathVariable("var") String var
+    ) {
+        return "another2";
     }
 
 }
